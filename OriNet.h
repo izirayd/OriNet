48b5c28489b043cef066e393878c7f1bd0e06edd
oriongame <izirayd@mail.ru>
2016-12-17 11:27:24 +0300

    Add files via upload

---
+++ OriNet.h
@@ -0,0 +1,14 @@
+﻿#ifndef OriNetH
+#define OriNetH
+
+#define OriVersion "0.6"
+
+#include "OriClient/OriClient.h"
+#include "OriServer/OriServer.h"
+#include "OriUntil/OriThread.h"
+#include "OriUntil/OriSleep.h"
+#include "OriUntil/OriConsole.h"
+#include "OriGeoIP/OriGeoIP.h"
+#include "OriCrypt/OriCrypt.h"
+
+#endif
